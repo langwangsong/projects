2b41ecdd856df87bf382e8ea7a516fab0c1fc97a
langwangsong <1095373086@qq.com>
2018-03-08 16:58:54 +0800

    hibernate01

---
+++ hibday01_01/src/cn/itcast/test/HibernateTest01.java
@@ -0,0 +1,98 @@
+package cn.itcast.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+
+import cn.itcast.domain.User;
+
+public class HibernateTest01 {
+	@Test
+	public void testAdd(){
+		/**
+		 * 1、加载hibernate配置文件及映射文件
+		 * 2、得到SessionFactory对象（好比数据库连接池）
+		 * 3、得到session对象（好比数据库连接Connection）
+		 * 4、开启事务
+		 * 5、执行CRUD操作
+		 * 6、提交或回滚事务
+		 * 7、关闭资源（session关闭，SessionFactory关闭）
+		 */
+		//1、加载hibernate配置文件及映射文件
+		Configuration cfg = new Configuration().configure();
+		//2、得到SessionFactory对象（好比数据库连接池）
+		SessionFactory sf = cfg.buildSessionFactory();
+		//3、得到session对象（好比数据库连接Connection）
+		Session session = sf.openSession();
+		//4、开启事务
+		Transaction tx = session.beginTransaction();
+		//5、执行CRUD操作
+		User u = new User("admin","admin");
+		session.save(u);
+		//6、提交或回滚事务 tx.rollback()
+		tx.commit();
+		//7、关闭资源
+		session.close();
+		sf.close();
+	}
+	@Test
+	public void testDelete(){
+		/**
+		 * 1、加载hibernate配置文件及映射文件
+		 * 2、得到SessionFactory对象（好比数据库连接池）
+		 * 3、得到session对象（好比数据库连接Connection）
+		 * 4、开启事务
+		 * 5、执行CRUD操作
+		 * 6、提交或回滚事务
+		 * 7、关闭资源（session关闭，SessionFactory关闭）
+		 */
+		//1、加载hibernate配置文件及映射文件
+		Configuration cfg = new Configuration().configure();
+		//2、得到SessionFactory对象（好比数据库连接池）
+		SessionFactory sf = cfg.buildSessionFactory();
+		//3、得到session对象（好比数据库连接Connection）
+		Session session = sf.openSession();
+		//4、开启事务
+		Transaction tx = session.beginTransaction();
+		//5、执行CRUD操作
+		User u =(User) session.get(User.class, "402881e4620499550162049956130000");//从数据库表中查询指定id的记录
+		session.delete(u);
+		//6、提交或回滚事务 tx.rollback()
+		tx.commit();
+		//7、关闭资源
+		session.close();
+		sf.close();
+	}
+	@Test
+	public void testUpdate(){
+		/**
+		 * 1、加载hibernate配置文件及映射文件
+		 * 2、得到SessionFactory对象（好比数据库连接池）
+		 * 3、得到session对象（好比数据库连接Connection）
+		 * 4、开启事务
+		 * 5、执行CRUD操作
+		 * 6、提交或回滚事务
+		 * 7、关闭资源（session关闭，SessionFactory关闭）
+		 */
+		//1、加载hibernate配置文件及映射文件
+		Configuration cfg = new Configuration().configure();
+		//2、得到SessionFactory对象（好比数据库连接池）
+		SessionFactory sf = cfg.buildSessionFactory();
+		//3、得到session对象（好比数据库连接Connection）
+		Session session = sf.openSession();
+		//4、开启事务
+		Transaction tx = session.beginTransaction();
+		//5、执行CRUD操作
+		User u =(User) session.get(User.class, "402881e4620499550162049956130000");//从数据库表中查询指定id的记录
+		u.setUsername("admin1");
+		u.setPassword("admin1");
+		session.update(u);
+		//6、提交或回滚事务 tx.rollback()
+		tx.commit();
+		//7、关闭资源
+		session.close();
+		sf.close();
+	}
+}
