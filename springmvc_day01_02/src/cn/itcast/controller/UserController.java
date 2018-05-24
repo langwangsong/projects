11a437ad699d129351fd0b26203bb4048b1150e2
langwangsong <1095373086@qq.com>
2018-05-24 20:23:19 +0800

    spring mvc day01-02

--- springmvc_day01_02/src/cn/itcast/controller/UserController.java
+++ springmvc_day01_02/src/cn/itcast/controller/UserController.java
@@ -4,14 +4,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.catalina.filters.AddDefaultCharsetFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
 
 import cn.itcast.domain.User;
 
